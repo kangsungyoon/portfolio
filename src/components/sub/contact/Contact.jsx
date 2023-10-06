811085dc3ae3a5757297627861ba25a71cbc2e42
kangsungyoon <ksy20051001@gmail.com>
2023-10-06 10:34:36 +0900

    restart

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -5,9 +5,10 @@ import { useRef, useEffect, useState } from 'react';
 
 export default function Contact() {
 	const map = useRef(null);
+	const view = useRef(null);
 	const instance = useRef(null);
 	const [Traffic, setTraffic] = useState(false);
-	const [Index, setIndex] = useState(0);
+	const [Index, setIndex] = useState(2);
 
 	const { kakao } = window;
 	//첫번째 지도를 출력하기 위한 객체정보
@@ -45,22 +46,41 @@ export default function Contact() {
 		),
 	});
 
+	const setCenter = () => {
+		// 지도 중심을 이동 시킵니다
+		instance.current.setCenter(info.current[Index].latlng);
+	};
+
 	useEffect(() => {
+		//Index값이 변경될때마다 새로운 지도 레이어가 중첩되므로
+		//일단은 기존 map안의 모든 요소를 없애서 초기화
+		map.current.innerHTML = '';
 		//객체 정보를 활용한 지도 객체 생성
 		instance.current = new kakao.maps.Map(map.current, {
 			center: info.current[Index].latlng,
 			level: 1,
 		});
-
 		//마커 객체에 지도 객체 연결
 		marker.setMap(instance.current);
-		//지도타입변경
+
+		//지도 타입 변경 UI추가
 		const mapTypeControl = new kakao.maps.MapTypeControl();
 		instance.current.addControl(mapTypeControl, kakao.maps.ControlPosition.BOTTOMLEFT);
+		window.addEventListener('resize', setCenter);
+
+		//로드뷰 관련 코드
+		const roadviewContainer = view.current;
+		const roadview = new kakao.maps.Roadview(roadviewContainer);
+		const roadviewClient = new kakao.maps.RoadviewClient();
+		const position = info.current[Index].latlng;
+
+		roadviewClient.getNearestPanoId(position, 50, (panoId) => {
+			roadview.setPanoId(panoId, position); //panoId와 중심좌표를 통해 로드뷰 실행
+		});
 	}, [Index]); //Index값이 변경될때마다 지도화면이 다시 갱신되어야 하므로 Index값을 의존성 배열에 등록
 
 	useEffect(() => {
-		//Traffic 값이 바뀔때마다 실행될 구문
+		//traffic 값이 바뀔때마다 실행될 구문
 		Traffic
 			? instance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
 			: instance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
@@ -68,18 +88,18 @@ export default function Contact() {
 
 	return (
 		<Layout title={'Contact'}>
-			{/* <button onClick={() => setTraffic(true)}>주변 교통정보 보기</button>
-			<button onClick={() => setTraffic(false)}>주변 교통정보 끄기</button> */}
-
 			<button onClick={() => setTraffic(!Traffic)}>
-				{Traffic ? '교통정보 끄기' : '교통정보 키기'}
+				{Traffic ? '교통정보 끄기' : '교통정보 켜기'}
 			</button>
 
+			<button onClick={setCenter}>지도 위치 초기화</button>
+
 			<div className='map' ref={map}></div>
+			<div className='view' ref={view}></div>
 
 			<ul>
 				{info.current.map((el, idx) => (
-					<li key={idx} onClick={() => setIndex(idx)}>
+					<li className={Index === idx ? 'on' : ''} key={idx} onClick={() => setIndex(idx)}>
 						{el.title}
 					</li>
 				))}
--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -19,9 +19,9 @@ export default function Youtube() {
 	const [Index, setIndex] = useState(0);
 
 	const fetchYoutube = () => {
-		const api_key = process.env.REACT_APP_YOUTUBE_API;
+		const api_key = 'AIzaSyDCGJcstcAwUWDoTcrQ4CZeSjdkDz8RSB4';
 		const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
-		const pid = 'PLxnkDxSlsKAFL-bto9b2pduWdqoYQazhW';
+		const pid = 'PLSflH3hv0IGX5YEKeboI4EFY3h5g6nFKU';
 		const num = 10;
 		const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 		fetch(resultURL)
