e53518d9ea421c244bb18000424551029b071846
kangsungyoon <ksy20051001@gmail.com>
2023-10-10 13:16:44 +0900

    이미지 에러이벤트 발생시 대체 이미지 출력

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,9 +1,10 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
+	const refInput = useRef(null);
 	const [Pics, setPics] = useState([]);
 	const my_id = '164021883@N04';
 
@@ -13,8 +14,7 @@ export default function Gallery() {
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
 		const method_search = 'flickr.photos.search';
-
-		const num = 50;
+		const num = 500;
 
 		//fetching함수 호출시 타입값이 있는 객체를 인수로 전달하면 해당 타입에 따라 호출 URL이 변경되고
 		//해당URL을 통해 받아지는 데이터로 달라짐
@@ -30,21 +30,33 @@ export default function Gallery() {
 
 		const data = await fetch(url);
 		const json = await data.json();
-		console.log(json.photos.photo);
+
+		if (json.photos.photo.length === 0) {
+			return alert('해당 검색어의 결과값이 없습니다.');
+		}
 		setPics(json.photos.photo);
 	};
 
 	useEffect(() => {
-		//type: 'interest' 인터레스트 방식 갤러리 호출
-		//type: 'user' 사용자 아이디 계정의 갤러리 호출
-		//type: 'search' 검색키워드로 갤러리 호출
-		//fetchData({ type: 'user', id: my_id });
-		//fetchData({ type: 'interest' });
-		fetchData({ type: 'search', tags: 'sky' });
+		fetchData({ type: 'user', id: my_id });
 	}, []);
 
 	return (
 		<Layout title={'Gallery'}>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					if (refInput.current.value.trim() === '') {
+						return alert('검색어를 입력하세요.');
+					}
+
+					fetchData({ type: 'search', tags: refInput.current.value });
+					refInput.current.value = '';
+				}}
+			>
+				<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
+				<button>검색</button>
+			</form>
 			<button onClick={() => fetchData({ type: 'user', id: my_id })}>My Gallery</button>
 			<button onClick={() => fetchData({ type: 'interest' })}>Interest Gallery</button>
 
@@ -70,6 +82,11 @@ export default function Gallery() {
 										<img
 											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
 											alt={data.owner}
+											onError={(e) => {
+												//만약 사용자가 프로필 이미지를 올리지 않았을때 엑박이 뜨므로
+												//onError이벤트를 연결해서 대체이미지 출력
+												e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
+											}}
 										/>
 										<span onClick={() => fetchData({ type: 'user', id: data.owner })}>
 											{data.owner}
