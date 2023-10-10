75949b3708fb341e3677409d8cb0c102a94a7776
kangsungyoon <ksy20051001@gmail.com>
2023-10-10 10:56:44 +0900

    사용자 아이디로 갤러리 추가 방식 개션

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,36 +1,78 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 import { useState, useEffect } from 'react';
+import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
-	const api_key = 'df39eea7518a5a4528b7bc5488282b35';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 500;
-	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+	const my_id = '164021883@N04';
+
+	const fetchData = async (opt) => {
+		let url = '';
+		const api_key = 'df39eea7518a5a4528b7bc5488282b35 ';
+		const method_interest = 'flickr.interestingness.getList';
+		const method_user = 'flickr.people.getPhotos';
+
+		const num = 50;
+
+		//fetching함수 호출시 타입값이 있는 객체를 인수로 전달하면 해당 타입에 따라 호출 URL이 변경되고
+		//해당URL을 통해 받아지는 데이터로 달라짐
+		if (opt.type === 'interest') {
+			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+		}
+		if (opt.type === 'user') {
+			url = `https://www.flickr.com/services/rest/?method=${method_user}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&user_id=${opt.id}`;
+		}
+
+		const data = await fetch(url);
+		const json = await data.json();
+		console.log(json.photos.photo);
+		setPics(json.photos.photo);
+	};
 
 	useEffect(() => {
-		fetch(url)
-			.then((data) => data.json())
-			.then((json) => {
-				console.log(json);
-				setPics(json.photos.photo);
-			});
+		//type: 'interest' 인터레스트 방식 갤러리 호출
+		//type: 'user' 사용자 아이디 계정의 갤러리 호출
+		fetchData({ type: 'user', id: my_id });
+		//fetchData({ type: 'interest' });
 	}, []);
 
 	return (
 		<Layout title={'Gallery'}>
-			{Pics.map((data, idx) => {
-				return (
-					<article key={idx}>
-						<h2>{data.title}</h2>
-						<img
-							src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
-							alt={data.title}
-						/>
-					</article>
-				);
-			})}
+			<button>My Gallery</button>
+			<button>Interest Gallery</button>
+
+			<div className='picFrame'>
+				<Masonry
+					elementType={'div'}
+					options={{ transitionDuration: '0.5s' }}
+					disableImagesLoaded={false}
+					updateOnEachImageLoad={false}
+				>
+					{Pics.map((data, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
+									<img
+										className='pic'
+										src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
+										alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
+									/>
+									<h2>{data.title}</h2>
+
+									<div className='profile'>
+										<img
+											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
+											alt={data.owner}
+										/>
+										<span>{data.owner}</span>
+									</div>
+								</div>
+							</article>
+						);
+					})}
+				</Masonry>
+			</div>
 		</Layout>
 	);
 }
