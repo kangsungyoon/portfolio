143e1cddf2aecc982e2f8c46ea770dd9a38d7bff
kangsungyoon <ksy20051001@gmail.com>
2023-10-12 09:26:23 +0900

    window 객체에 연결된 핸들러 함수는 무조건 해당 컴포넌트 언마운트시 반드시 제거

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,23 +1,24 @@
 import Layout from '../../common/layout/Layout';
-import emailjs from '@emailjs/browser';
 import './Contact.scss';
+import emailjs from '@emailjs/browser';
 import { useRef, useEffect, useState } from 'react';
+
 export default function Contact() {
+	const form = useRef(null);
 	const map = useRef(null);
 	const view = useRef(null);
 	const instance = useRef(null);
-	const form = useRef(null);
 	const [Traffic, setTraffic] = useState(false);
 	const [Index, setIndex] = useState(0);
 	const [IsMap, setIsMap] = useState(true);
 
-	// kako Api를 cdn 방식으로 불러오고 있기 떄문에 리액트 컴포넌트가 실행되면 window객체에서 직접 비구조화 할당으로 kakao 객체를 뽑아옴
+	//kakao api를 cdn방식으로 불러오고 있기 때문에 리액트 컴포넌트가 실행되면 window객체에서 직접 비구조화 할당으로 kakao객체를 뽑아옴
 	const { kakao } = window;
 	//첫번째 지도를 출력하기 위한 객체정보
 
-	// 지도정보데이터를 객체형식으로 구조화한 다음에 데이터 기반으로 자동 지도화면이 생성되도록 만들었다.
-	// 데이터 정보가 많아질때를 대비해서 유지보수에 최적화 되도록 코드 개선
-	// 해당 정보값은 자주 바뀌는값이 아니기 떄문에 굳이 state에 담아서 불필요한 재랜더링을 막기 위해 useRef에 담아놨다
+	//지도정보데이터를 객체형식으로 구조화한 다음에 데이터 기반으로 자동 지도화면이 생성되도록 만들었다.
+	//데이터정보가 많아질때를 대비해서 유지보수에 최적화되도록 코드 개선
+	//해당 정보값은 자주 바뀌는값이 아니기 때문에 굳이 state에 담아서 불필요한 재랜더링을 막기위해 useRef에 담아놨다
 	const info = useRef([
 		{
 			title: '삼성역 코엑스',
@@ -41,6 +42,7 @@ export default function Contact() {
 			imgPos: { offset: new kakao.maps.Point(116, 99) },
 		},
 	]);
+
 	//위의 정보값을 활용한 마커 객체 생성
 	const marker = new kakao.maps.Marker({
 		position: info.current[Index].latlng,
@@ -50,12 +52,15 @@ export default function Contact() {
 			info.current[Index].imgPos
 		),
 	});
+
+	//지도위치를 중심으로 이동시키는 핸들러 함수 제작
 	const setCenter = () => {
 		// 지도 중심을 이동 시킵니다
 		instance.current.setCenter(info.current[Index].latlng);
 	};
+
 	useEffect(() => {
-		//index값이 변경될떄마다 새로운 지도 레이어가 중첩되므로
+		//Index값이 변경될때마다 새로운 지도 레이어가 중첩되므로
 		//일단은 기존 map안의 모든 요소를 없애서 초기화
 		map.current.innerHTML = '';
 		//객체 정보를 활용한 지도 객체 생성
@@ -65,25 +70,40 @@ export default function Contact() {
 		});
 		//마커 객체에 지도 객체 연결
 		marker.setMap(instance.current);
-		//지도타입변경
+
+		//지도 타입 변경 UI추가
 		const mapTypeControl = new kakao.maps.MapTypeControl();
 		instance.current.addControl(mapTypeControl, kakao.maps.ControlPosition.BOTTOMLEFT);
+
+		// 지도 생성시 마커 고정적으로 적용되기 때문에 브라우저 리사이즈시 마커가 가운데 위치하지 않는 문제
+		// 마커를 가운데 고정 시키는 함수를 제작한뒤 윈도우 객체 직접 resize이벤트 발생시마다 핸들러 함수 호출해서 마커위치 보정
+
+		// Contact페이지에만 동작되야 되는 핸들러 함수를 최상위 객체인 window에 직접 연결했기 때문에
+		// 라우터로 다른페이지 이동하더라도 계속해서 setCenter호출되는 문제 발생
+		// 해결방법 : Contact 컴포넌트가 언마운트시 강제로 윈도우 객체에서 setCenter핸들러를 제가
 		window.addEventListener('resize', setCenter);
+
 		//로드뷰 관련 코드
 		new kakao.maps.RoadviewClient().getNearestPanoId(
 			info.current[Index].latlng,
-			50, //해당 지도의 위치값에서 반경 50미터 안에 제일 가까운 도로 기준으로 로드뷰화면 생성
+			100, //해당 지도의 위치값에서 반경 100미터 안에 제일 가까운 도로 기준으로 로드뷰화면 생성
 			(panoId) => {
 				new kakao.maps.Roadview(view.current).setPanoId(panoId, info.current[Index].latlng);
 			}
 		);
+
+		return () => {
+			window.addEventListener('resize', setCenter);
+		};
 	}, [Index]); //Index값이 변경될때마다 지도화면이 다시 갱신되어야 하므로 Index값을 의존성 배열에 등록
+
 	useEffect(() => {
-		//Traffic 값이 바뀔때마다 실행될 구문
+		//traffic 값이 바뀔때마다 실행될 구문
 		Traffic
 			? instance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
 			: instance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
+
 	const resetForm = () => {
 		const nameForm = form.current.querySelector('.nameEl');
 		const mailForm = form.current.querySelector('.emailEl');
@@ -92,16 +112,21 @@ export default function Contact() {
 		mailForm.value = '';
 		msgForm.value = '';
 	};
+
 	//form mail 기능함수
 	const sendEmail = (e) => {
 		e.preventDefault();
+
 		const nameForm = form.current.querySelector('.nameEl');
 		const mailForm = form.current.querySelector('.emailEl');
 		const msgForm = form.current.querySelector('.msgEl');
+
 		if (!nameForm.value || !mailForm.value || !msgForm.value)
-			return alert('사용자이름,이메일주소,문의내용은 필수 입력사항입니다');
-		//sendForm메서드는 각 키값을 문자열로만 인수로 전달되도록 type지정되어 있기 떄문에
+			return alert('사용자이름, 이메일주소, 문의내용은 필수 입력사항입니다.');
+
+		//sendForm메서드는 각 키값을 문자열로만 인수로 전달되도록 type지정되어 있기 때문에
 		//변수를 `${}`로 감싸서 문자형식으로 전달
+
 		emailjs
 			.sendForm(
 				`${process.env.REACT_APP_SERVICE_ID}`,
@@ -122,38 +147,49 @@ export default function Contact() {
 				}
 			);
 	};
+
 	return (
 		<Layout title={'Contact'}>
-			{/* <button onClick={() => setTraffic(true)}>주변 교통정보 보기</button>
-      <button onClick={() => setTraffic(false)}>주변 교통정보 끄기</button> */}
-			<div id='mailbox'>
+			<div id='mailBox'>
 				<form ref={form} onSubmit={sendEmail}>
 					<div className='upper'>
-						<label>Name</label>
-						<input type='text' name='user_name' className='nameEl' />
-						<label>Email</label>
-						<input type='email' name='user_email' className='emailEl' />
+						<span>
+							<label>Name</label>
+							<input type='text' name='user_name' className='nameEl' />
+						</span>
+
+						<span>
+							<label>Email</label>
+							<input type='email' name='user_email' className='emailEl' />
+						</span>
 					</div>
+
 					<div className='lower'>
 						<label>Message</label>
 						<textarea name='message' className='msgEl' />
 					</div>
-					<div className='btnSer'>
+
+					<div className='btnSet'>
 						<input type='reset' value='Cancel' />
 						<input type='submit' value='Send' />
 					</div>
 				</form>
 			</div>
+
 			<div id='mapBox'>
 				<button onClick={() => setTraffic(!Traffic)}>
-					{Traffic ? '교통정보 끄기' : '교통정보 키기'}
+					{Traffic ? '교통정보 끄기' : '교통정보 켜기'}
 				</button>
+
 				<button onClick={setCenter}>지도 위치 초기화</button>
-				<button onClick={() => setIsMap(!IsMap)}> {IsMap ? '로드뷰보기' : '지도보기'}</button>
+				<button onClick={() => setIsMap(!IsMap)}>{IsMap ? '로드뷰보기' : '지도보기'}</button>
+
 				<div className='container'>
 					<div className={`view ${IsMap ? '' : 'on'}`} ref={view}></div>
 					<div className={`map ${IsMap ? 'on' : ''}`} ref={map}></div>
 				</div>
+
+				{/* 데이터기반으로 자동 버튼 생성 및 자동 이벤트 연결 처리 */}
 				<ul>
 					{info.current.map((el, idx) => (
 						<li
