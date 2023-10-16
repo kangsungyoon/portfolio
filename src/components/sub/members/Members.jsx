7328069ae1836659f33fbf5497e2aa18831fded0
kangsungyoon <ksy20051001@gmail.com>
2023-10-16 10:14:57 +0900

    handlechage핸들러 모든 인풋에 연결

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -1,7 +1,21 @@
 import Layout from '../../common/layout/Layout';
 import './Members.scss';
+import { useState } from 'react';
 
 export default function Members() {
+	const initVal = {
+		userid: '',
+		pwd1: '',
+		pwd2: '',
+		email: '',
+	};
+	const [Val, setVal] = useState(initVal);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		console.log(name, value);
+	};
+
 	return (
 		<Layout title={'Members'}>
 			<form>
@@ -9,39 +23,74 @@ export default function Members() {
 					<legend className='h'>회원가입 폼 양식</legend>
 					<table border='1'>
 						<tbody>
-							{/* userid*/}
+							{/* userid */}
 							<tr>
 								<th scope='row'>
 									<label htmlFor='userid'>userid</label>
 								</th>
 								<td>
-									<input type='text' id='userid' name='userid' />
+									<input
+										type='text'
+										id='userid'
+										name='userid'
+										value={Val.userid}
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 
-							{/* password*/}
+							{/* password */}
 							<tr>
 								<th scope='row'>
 									<label htmlFor='pwd1'>password</label>
 								</th>
 								<td>
-									<input type='password' id='pwd1' name='pwd1' />
+									<input
+										type='password'
+										id='pwd1'
+										name='pwd1'
+										value={Val.pwd1}
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
-							{/* password2*/}
+
+							{/* re password */}
 							<tr>
 								<th scope='row'>
 									<label htmlFor='pwd2'>re-password</label>
 								</th>
 								<td>
-									<input type='password' id='pwd2' name='pwd2' />
+									<input
+										type='password'
+										id='pwd2'
+										name='pwd2'
+										value={Val.pwd2}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+
+							{/* email */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='email'>e-mail</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='email'
+										name='email'
+										value={Val.email}
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 
 							{/* btnSet */}
 							<tr>
 								<th colSpan='2'>
-									<input type='reset' Value='cancel' />
+									<input type='reset' value='cancel' />
 									<input type='submit' value='send' />
 								</th>
 							</tr>
