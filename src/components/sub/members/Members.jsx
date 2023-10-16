a398a2aac6ef1307ecf6467af427c7a1bfe25525
kangsungyoon <ksy20051001@gmail.com>
2023-10-16 15:02:44 +0900

    인풋요소 초기화

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -8,30 +8,106 @@ export default function Members() {
 		pwd1: '',
 		pwd2: '',
 		email: '',
+		gender: false,
+		interests: false,
+		edu: '',
+		comments: '',
 	};
 	const [Val, setVal] = useState(initVal);
+	const [Errs, setErrs] = useState({});
+
+	const resetForm = (e) => {
+		e.preventDefault();
+		setVal(initVal);
+	};
 
 	const handleChange = (e) => {
 		const { name, value } = e.target;
 		setVal({ ...Val, [name]: value });
 	};
 
-	//인수값으로 state를 전달받아서 각 데이터별로 인증처리후
-	//만약 인증에러가 발생하면 해당 name값으로 에러문구를 생성해서 반환하는 함수
+	const handleRadio = (e) => {
+		const { name, checked } = e.target;
+		setVal({ ...Val, [name]: checked });
+	};
+
+	const handleCheck = (e) => {
+		const { name } = e.target;
+		let isChecked = false;
+		const inputs = e.target.parentElement.querySelectorAll('input');
+		inputs.forEach((input) => input.checked && (isChecked = true));
+		setVal({ ...Val, [name]: isChecked });
+	};
+
 	const check = (value) => {
+		const num = /[0-9]/; //0-9까지의 모든 값을 정규표현식으로 범위지정
+		const txt = /[a-zA-Z]/; //대소문자 구분없이 모든 문자 범위지정
+		const spc = /[!@#$%^*()_]/; //모든 특수문자 지정
 		const errs = {};
+
 		if (value.userid.length < 5) {
-			errs.userid = '아이디는 최소 5글자 이상 입력하세요';
+			errs.userid = '아이디는 최소 5글자 이상 입력하세요.';
+		}
+
+		//비밀번호 인증 (5글자 이상, 문자, 숫자, 특수문자 모두 포함)
+		if (
+			value.pwd1.length < 5 ||
+			!num.test(value.pwd1) ||
+			!txt.test(value.pwd1) ||
+			!spc.test(value.pwd1)
+		) {
+			errs.pwd1 = '비밀번호는 5글자이상, 문자,숫자,특수문자를 모두 포함하세요';
+		}
+
+		//비밀번호 재확인 인증
+		if (value.pwd1 !== value.pwd2 || !value.pwd2) {
+			errs.pwd2 = '2개의 비밀번호를 같게 입력하세요.';
+		}
+
+		//이메일 인증
+		if (!value.email || !/@/.test(value.email)) {
+			errs.email = '이메일은 무조건 @를 포함해야 합니다.';
+		} else {
+			const [forward, backward] = value.email.split('@');
+			if (!forward || !backward) {
+				errs.email = '이메일에 @앞뒤로 문자값이 있어야 합니다.';
+			} else {
+				const [forward, backward] = value.email.split('.');
+				if (!forward || !backward) {
+					errs.email = '이메일 . 앞뒤로 문자값이 있어야 합니다.';
+				}
+			}
+		}
+
+		//성별인증
+		if (!value.gender) {
+			errs.gender = '성별은 필수 체크항목입니다.';
+		}
+
+		//관심사인증
+		if (!value.interests) {
+			errs.interests = '관심사를 하나이상 체크해주세요.';
+		}
+
+		//학력 인증
+		if (!value.edu) {
+			errs.edu = '학력을 선택하세요.';
+		}
+		//남기는말 인증
+		if (value.comments.length < 10) {
+			errs.comments = '남기는말은 10글자 이상 입력하세요.';
 		}
 		return errs;
 	};
 
-	//전송이벤트 발생시 state에 있는 인풋값들을 check함수에 전달해서 호출
-	//만약 check함수가 에러객체를 하나도 내보내지 않으면 인증성공
-	//하나라도 에러객체가 전달되면 인증실패처리하면서 name값과 매칭이 되는 input요소 아래쪽에 에러메세지 출력
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(check(Val));
+
+		if (Object.keys(check(Val)).length === 0) {
+			alert('인증통과');
+		} else {
+			setErrs(check(Val));
+		}
 	};
 
 	return (
@@ -54,6 +130,7 @@ export default function Members() {
 										value={Val.userid}
 										onChange={handleChange}
 									/>
+									{Errs.userid && <p>{Errs.userid}</p>}
 								</td>
 							</tr>
 
@@ -70,6 +147,7 @@ export default function Members() {
 										value={Val.pwd1}
 										onChange={handleChange}
 									/>
+									{Errs.pwd1 && <p>{Errs.pwd1}</p>}
 								</td>
 							</tr>
 
@@ -86,6 +164,7 @@ export default function Members() {
 										value={Val.pwd2}
 										onChange={handleChange}
 									/>
+									{Errs.pwd2 && <p>{Errs.pwd2}</p>}
 								</td>
 							</tr>
 
@@ -102,13 +181,78 @@ export default function Members() {
 										value={Val.email}
 										onChange={handleChange}
 									/>
+									{Errs.email && <p>{Errs.email}</p>}
+								</td>
+							</tr>
+
+							{/* gender */}
+							<tr>
+								<th>gender</th>
+								<td>
+									<label htmlFor='female'>female</label>
+									<input type='radio' name='gender' id='female' onChange={handleRadio} />
+
+									<label htmlFor='male'>male</label>
+									<input type='radio' name='gender' id='male' onChange={handleRadio} />
+									{Errs.gender && <p>{Errs.gender}</p>}
+								</td>
+							</tr>
+
+							{/* interests */}
+							<tr>
+								<th>interests</th>
+								<td>
+									<label htmlFor='sports'>sports</label>
+									<input type='checkbox' id='sports' name='interests' onChange={handleCheck} />
+
+									<label htmlFor='game'>game</label>
+									<input type='checkbox' id='game' name='interests' onChange={handleCheck} />
+
+									<label htmlFor='music'>music</label>
+									<input type='checkbox' id='music' name='interests' onChange={handleCheck} />
+									{Errs.interests && <p>{Errs.interests}</p>}
+								</td>
+							</tr>
+
+							{/* education */}
+							<tr>
+								<th>
+									<label htmlFor='edu'>Education</label>
+								</th>
+								<td>
+									<select name='edu' id='edu' onChange={handleChange}>
+										<option value=''>최종학력 선택하세요</option>
+										<option value='elementary-school'>초등학교 졸업</option>
+										<option value='middle-school'>중학교 졸업</option>
+										<option value='high-school'>고등학교 졸업</option>
+										<option value='college'>대학교 졸업</option>
+									</select>
+									{Errs.edu && <p>{Errs.edu}</p>}
+								</td>
+							</tr>
+
+							{/* comments */}
+							<tr>
+								<th>
+									<label htmlFor='comments'>comments</label>
+								</th>
+								<td>
+									<textarea
+										name='comments'
+										id=''
+										cols='30'
+										rows='3'
+										value={Val.comments}
+										onChange={handleChange}
+									></textarea>
+									{Errs.comments && <p>{Errs.comments}</p>}
 								</td>
 							</tr>
 
 							{/* btnSet */}
 							<tr>
 								<th colSpan='2'>
-									<input type='reset' value='cancel' />
+									<input type='reset' value='cancel' onClick={resetForm} />
 									<input type='submit' value='send' />
 								</th>
 							</tr>
