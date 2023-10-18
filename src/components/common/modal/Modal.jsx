730f1621afedc2556be99ec676d73c4533209645
kangsungyoon <ksy20051001@gmail.com>
2023-10-18 11:31:22 +0900

    썸네일 클릭시 팝업 오류 버전

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -1,7 +1,10 @@
 import './Modal.scss';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { close } from '../../../redux/modalSlice';
 
-function Modal({ children, setIsModal }) {
+const Modal = ({ children }) => {
+	const dispatch = useDispatch();
 	useEffect(() => {
 		document.body.style.overflow = 'hidden';
 
@@ -12,8 +15,9 @@ function Modal({ children, setIsModal }) {
 	return (
 		<aside className='modal'>
 			<div className='con'>{children}</div>
-			<span onClick={() => setIsModal(false)}>close</span>
+			<span onClick={() => dispatch(close())}>close</span>
 		</aside>
 	);
-}
+};
+
 export default Modal;
