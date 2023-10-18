44a6076a9bb89dd5020df0dce22d95c139c3a186
kangsungyoon <ksy20051001@gmail.com>
2023-10-18 09:54:19 +0900

    메인페이지 flickr 전역 데이터 출력

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -9,30 +9,30 @@ export default function Youtube() {
 	return (
 		<>
 			<Layout title={'Youtube'}>
-				{Youtube.map((data, idx) => {
-					let tit = data.snippet.title;
-					let desc = data.snippet.description;
-					let date = data.snippet.publishedAt;
+				<div className='wrab'>
+					{Youtube.map((data, idx) => {
+						let tit = data.snippet.title;
+						let desc = data.snippet.description;
+						let date = data.snippet.publishedAt;
 
-					return (
-						<article key={idx}>
-							<div className='titBox'>
-								<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
-							</div>
-
-							<div className='conBox'>
-								<p>{desc.length > 180 ? desc.substr(0, 180) + '...' : desc}</p>
-								<span>{date.split('T')[0].split('-').join('.')}</span>
-							</div>
-
-							<div className='picBox'>
-								<Link to={`/detail/${data.id}`}>
-									<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
-								</Link>
-							</div>
-						</article>
-					);
-				})}
+						return (
+							<article key={idx}>
+								<div className='picBox'>
+									<Link to={`/detail/${data.id}`}>
+										<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
+									</Link>
+								</div>
+								<div className='titBox'>
+									<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
+								</div>
+								<div className='conBox'>
+									<p>{desc.length > 180 ? desc.substr(0, 180) + '...' : desc}</p>
+									<span>{date.split('T')[0].split('-').join('.')}</span>
+								</div>
+							</article>
+						);
+					})}
+				</div>
 			</Layout>
 
 			{/* {IsModal && (
