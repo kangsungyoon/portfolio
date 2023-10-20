b3a3a966036cc3890cdf3c96057be98e6878ab47
kangsungyoon <ksy20051001@gmail.com>
2023-10-20 16:07:21 +0900

    youtube 서브페이지 리액트 쿼리로 변경

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -2,37 +2,38 @@
 import Layout from '../../common/layout/Layout';
 import './Youtube.scss';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useYoutubeQuery } from '../../../hooks/useYoutube';
 
 export default function Youtube() {
-	const Youtube = useSelector((store) => store.youtube.data);
+	const { data: Youtube, isSuccess } = useYoutubeQuery();
 
 	return (
 		<>
 			<Layout title={'Youtube'}>
 				<div className='wrab'>
-					{Youtube.map((data, idx) => {
-						let tit = data.snippet.title;
-						let desc = data.snippet.description;
-						let date = data.snippet.publishedAt;
+					{isSuccess &&
+						Youtube.map((data, idx) => {
+							let tit = data.snippet.title;
+							let desc = data.snippet.description;
+							let date = data.snippet.publishedAt;
 
-						return (
-							<article key={idx}>
-								<div className='picBox'>
-									<Link to={`/detail/${data.id}`}>
-										<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
-									</Link>
-								</div>
-								<div className='titBox'>
-									<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
-								</div>
-								<div className='conBox'>
-									<p>{desc.length > 180 ? desc.substr(0, 180) + '...' : desc}</p>
-									<span>{date.split('T')[0].split('-').join('.')}</span>
-								</div>
-							</article>
-						);
-					})}
+							return (
+								<article key={idx}>
+									<div className='picBox'>
+										<Link to={`/detail/${data.id}`}>
+											<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
+										</Link>
+									</div>
+									<div className='titBox'>
+										<h2>{tit.length > 60 ? tit.substr(0, 60) + '...' : tit}</h2>
+									</div>
+									<div className='conBox'>
+										<p>{desc.length > 180 ? desc.substr(0, 180) + '...' : desc}</p>
+										<span>{date.split('T')[0].split('-').join('.')}</span>
+									</div>
+								</article>
+							);
+						})}
 				</div>
 			</Layout>
 		</>
--- src/hooks/useYoutube.js
+++ src/hooks/useYoutube.js
@@ -4,8 +4,8 @@ import { useQuery } from '@tanstack/react-query';
 const fetchYoutube = async () => {
 	const api_key = process.env.REACT_APP_YOUTUBE_API;
 	const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
-	const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
-	const num = 10;
+	const pid = 'PLSflH3hv0IGX5YEKeboI4EFY3h5g6nFKU';
+	const num = 6;
 	const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 
 	const { data } = await axios.get(resultURL);
@@ -14,7 +14,18 @@ const fetchYoutube = async () => {
 
 //react-query에는 쿼리키를 문자열로 지정해서 데이터 호출시 쿼리키가 동일하면 동일한 데이터로 인지해서
 //refetching처리하지 않고 캐싱되어 있는 데이터를 재활용
-//useQuery([쿼리키], fetching함수, 캐싱옵션)
+//useQuery([쿼리키], fetching함수, 캐싱옵션)j
+//react-query에서 쓰이는 데이터 상태 개념
+
+//pending : 데이터 요청을 보내고 응답을 받기까지의 상태
+//fresh : 작업자가 직접 지정한 데이터의 신선한 상태
+//stale : 작업자가 직접 지정한 데이터의 신선하지 못한 옛날 데이터의 상태
+//inactive : 해당 컴포넌트에서 리액트쿼리고 관리하고 있는 데이터를 활용하고 있지 않은 비활성화 상태
 export const useYoutubeQuery = () => {
-	return useQuery(['youtubeData'], fetchYoutube);
+	return useQuery(['youtubeData'], fetchYoutube, {
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+		cacheTime: 1000 * 60 * 60,
+		staleTime: 1000 * 60 * 60,
+	});
 };
