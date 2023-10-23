95ceb66a58231b2194451abf58bc71f26942e6aa
kangsungyoon <ksy20051001@gmail.com>
2023-10-23 11:11:47 +0900

    테마 버튼 토글시 테마 클래스 변경

--- src/App.js
+++ src/App.js
@@ -11,17 +11,20 @@ import Detail from './components/sub/youtube/Detail';
 import Community from './components/sub/community/Community';
 import Main from './components/main/mainWrap/Main';
 import { useMedia } from './hooks/useMedia';
+
 import Menu from './components/common/menu/Menu';
 import Footer from './components/common/footer/Footer';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useGlobalData } from './hooks/useGlobalContext';
 
 function App() {
 	const queryClient = new QueryClient();
+	const { Theme } = useGlobalData();
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<main className={useMedia()}>
+			<main className={`${useMedia()} ${Theme ? 'dark' : 'light'}`}>
 				<Switch>
 					<Route exact path='/'>
 						<Header isMain={true} />
